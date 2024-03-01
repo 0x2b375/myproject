b1ff70eff1464c8529aa879ecb2e26bdf35c5047
0xb39 <giiguu.giigu@gmail.com>
2024-03-01 18:58:37 +0800

    added paho-mqtt

--- main/views.py
+++ main/views.py
@@ -4,12 +4,6 @@ from django.http import JsonResponse
 from myproject.mqtt import client as mqtt_client
 # Create your views here.
 
-
-
 def index(request):
+  mqtt_client.publish('0aefjk5643/', 'HELLO !!!')
   return render(request, 'main/index.html')
-
-def publish_message(request):
-  request_data = json.loads(request.body)
-  rc, mid = mqtt_client.publish(request_data['topic'], request_data['msg'])
-  return JsonResponse({'code': rc})
--- myproject/mqtt.py
+++ myproject/mqtt.py
@@ -8,14 +8,12 @@ def on_connect(mqtt_client, userdata, flags, rc, properties):
   else:
     print('Failed to connect!')    
 
-   
-
-# The callback for when a PUBLISH message is received from the server.
+  
 def on_message(client, userdata, msg):
-    print(msg.topic+" "+str(msg.payload))
+    print(f'Received message on topic: {msg.topic} with payload: {str(msg.payload)}')
     
 
-client = mqtt.Client()
+client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
 client.on_connect = on_connect
 client.on_message = on_message
 client.username_pw_set(settings.MQTT_USER, settings.MQTT_PASSWORD)
@@ -23,5 +21,5 @@ client.username_pw_set(settings.MQTT_USER, settings.MQTT_PASSWORD)
 client.connect(
   host = settings.MQTT_SERVER,
   port = settings.MQTT_PORT,
-  keepalive = settings.KEEP_ALIVE
+  keepalive = settings.MQTT_KEEPALIVE
 )
