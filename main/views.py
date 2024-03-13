f7e0d54b774a5161f94df2b3fad2658a41fba615
0xb39 <giiguu.giigu@gmail.com>
2024-03-13 13:40:53 +0800

    fixed cmd_center

--- main/views.py
+++ main/views.py
@@ -5,7 +5,6 @@ from myproject.mqtt import client as mqtt_client
 # Create your views here.
 
 def index(request):
-  mqtt_client.publish('0aefjk5643/', 'HELLO FROM DJANGO')
   return render(request, 'main/index.html')
 
 def command_center(request):
